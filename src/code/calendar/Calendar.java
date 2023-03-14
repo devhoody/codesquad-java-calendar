477fc19526ef03b4a7ba4296ceea20443b242063
devhoody <rethnoly@gmail.com>
2023-03-14 16:10:53 +0900

    implement maxdays of month

--- src/code/calendar/Calendar.java
+++ src/code/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package code.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,6 +11,20 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-	}
+		
+		System.out.println("원하는 달을 입력하세요.");
 
+		int[] maxdays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		
+		Scanner scn = new Scanner(System.in);
+		
+		int month = scn.nextInt();
+		
+		int mon2 = maxdays[month-1];
+		
+		System.out.printf("%d월의 마지막 일수는 %d일 입니다.\n", month, maxdays[month-1]);
+		
+		scn.close();
+	}
+		
 }
