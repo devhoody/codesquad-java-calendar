820a19098d05ca86b092b0eddd34ca0e9a4729d8
devhoody <rethnoly@gmail.com>
2023-03-14 17:13:18 +0900

    update calendar

--- src/code/calendar/Calendar.java
+++ src/code/calendar/Calendar.java
@@ -23,15 +23,22 @@ public class Calendar {
 		//원하는 반복 횟수를 입력하고, 원하는 월수를 입력하여 해당 월의 마지막 일수 출력하기.
 		Scanner scn = new Scanner(System.in);
 		Calendar cal = new Calendar();
+		String prompt = "cal>";
 		
 		System.out.println("반복횟수를 입력하세요.");
 		int loopnum = scn.nextInt();
-		for (int i=0; i<loopnum; i++) {
-			System.out.println("원하는 달을 입력하세요.");
-			int month = scn.nextInt();
-			System.out.printf("%d월의 마지막 일수는 %d일 입니다.\n", month, cal.getMaxDaysofMonth(month));
+		
+		if (loopnum != -1) {
+			for (int i=0; i<loopnum; i++) {
+				System.out.println("원하는 달을 입력하세요.");
+				System.out.print(prompt);
+				int month = scn.nextInt();
+				System.out.printf("%d월의 마지막 일수는 %d일 입니다.\n", month, cal.getMaxDaysofMonth(month));
+			}
+		} else {
+			System.out.println("Bye");
 		}
-
+		
 		scn.close();
 	}
 
